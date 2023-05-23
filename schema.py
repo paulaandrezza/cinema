965f8aed3ccebc0f3d9af2eb8d321964ff44dd96
paulaandrezza <paulaandrezza25@gmail.com>
2023-05-23 18:29:38 -0300

    config schema

---
+++ schema.py
@@ -0,0 +1,9 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def criar_banco(banco):
+    # Definindo variavel com a conexao com o BD.
+    conn = sqlite3.connect(banco)
+    # Definindo cursor para manipular dados do BD.
+    c = conn.cursor()
