0f94cf9e42b2833f4fa454ecff2832f9dc3812f9
paulaandrezza <paulaandrezza25@gmail.com>
2023-05-23 18:47:16 -0300

    Iniciando schema.py

--- schema.py
+++ schema.py
@@ -3,7 +3,21 @@ from sqlite3 import Error
 
 
 def criar_banco(banco):
-    # Definindo variavel com a conexao com o BD.
-    conn = sqlite3.connect(banco)
-    # Definindo cursor para manipular dados do BD.
-    c = conn.cursor()
+  # Definindo variavel com a conexao com o BD.
+  conn = sqlite3.connect(banco)
+  # Definindo cursor para manipular dados do BD.
+  c = conn.cursor()
+
+  try:
+    c.execute("""
+    
+    """)
+
+    c.execute("""
+    """)
+
+    print("Tabelas criadas com sucesso!")
+    return conn
+  
+  except Error as e:
+    print(e)
