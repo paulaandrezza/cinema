24d7dca2c395eb96b85a5c7d3af449f65ad084ef
ThaisaVasconcelos <thaisasvasconcelos@gmail.com>
2023-05-30 19:55:20 -0300

    finalizando shcema.py

--- schema.py
+++ schema.py
@@ -10,10 +10,27 @@ def criar_banco(banco):
 
   try:
     c.execute("""
-    
+    CREATE TABLE "FILME" ("idFilme"	INTEGER NOT NULL,	"nome"	TEXT NOT NULL,	"descricao"	TEXT NOT NULL,	"genero"	TEXT NOT NULL, "duracao"	INTEGER NOT NULL,	"classificacao"	NUMERIC NOT NULL,	PRIMARY KEY("idFilme"));
     """)
 
     c.execute("""
+    CREATE TABLE "FUNCIONARIO" ("idUsuario"	INTEGER NOT NULL, "login"	TEXT NOT NULL, "senha"	TEXT NOT NULL, "nome"	INTEGER NOT NULL, "idUnidade"	INTEGER NOT NULL, FOREIGN KEY("idUnidade") REFERENCES "UNIDADE"("idUnidade"), PRIMARY KEY("idUsuario" AUTOINCREMENT));
+    """)
+
+    c.execute("""
+    CREATE TABLE "INGRESSO" ("idingresso"	INTEGER NOT NULL,	"nomeCliente"	TEXT NOT NULL,	"idSessao"	INTEGER NOT NULL, PRIMARY KEY("idingresso"), FOREIGN KEY("idSessao") REFERENCES "SESSAO"("idSessao"));
+    """)
+
+    c.execute("""
+    CREATE TABLE "SALA" ("idSala"	INTEGER NOT NULL, "capacidadeAssentos"	INTEGER NOT NULL,	"idUnidade"	INTEGER,	PRIMARY KEY("idSala"),	FOREIGN KEY("idUnidade") REFERENCES "UNIDADE"("idUnidade"));
+    """)
+
+    c.execute("""
+    CREATE TABLE "SESSAO" ("idSessao"	INTEGER NOT NULL,	"horario"	TEXT NOT NULL,	"data"	TEXT NOT NULL,	"idFilme"	INTEGER NOT NULL,	"idSala"	INTEGER NOT NULL,	PRIMARY KEY("idSessao" AUTOINCREMENT),	FOREIGN KEY("idSala") REFERENCES "SALA"("idSala"),	FOREIGN KEY("idFilme") REFERENCES "FILME"("idFilme"));
+    """)
+
+    c.execute("""
+    CREATE TABLE "UNIDADE" ("idUnidade"	INTEGER NOT NULL,	"estado"	TEXT NOT NULL,	"cidade"	TEXT NOT NULL,	"bairro"	TEXT NOT NULL,	"logradouro"	TEXT NOT NULL,	"numero"	INTEGER NOT NULL,	PRIMARY KEY("idUnidade"));
     """)
 
     print("Tabelas criadas com sucesso!")
